0dd6fc0760e933db6e300047fb76b5b28ec15be1
Daniel-Steven-Holguin-Soto <holguinsotodanielsteven@gmail.com>
2025-12-02 06:39:31 -0500

    ci: agregar workflow de CI, Prettier y test minimo

--- features/calculadora.js
+++
@@ -1,8 +0,0 @@
-// Definimos la función
-function sumar(a, b) {
-  return a + b;
-}
-
-// Ejemplo de uso
-let resultado = sumar(5, 3);
-console.log("El resultado de la suma es:", resultado);
--- utils/validation.js
+++
@@ -1,4 +0,0 @@
-export function ValidationEmail(email) {
-    if (!email) return false;
-    return email.includes('@');
-}
